baa4145e871567ee3afbbc0910aa1f9446058819
Bronson Quick <bronson@bronsonquick.com.au>
2019-01-05 14:10:15 +1100

    Add restart command

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const execSync = require('child_process').execSync;
 const colors = require('colors');
+colors.setTheme( require( '../themes/logging.js' ) );
+
+module.exports = function restart() {
+    let cwd = process.cwd();
+    console.log( colors.warning( 'Restarting docker containers...' ) );
+    try {
+        execSync( `docker-compose restart`, { stdio: 'inherit', cwd: cwd });
+        console.log( colors.success( 'Succussfully restarted Docker containers.' ));
+    } catch (ex) {
+        console.log( colors.error( 'Failed to restart Docker containers.' ) );
+    }
+};
 
 // Run `docker-compose restart -d`
