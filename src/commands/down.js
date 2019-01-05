17af71b6eaeea0572f511467d04aa41e3acb7c28
Bronson Quick <bronson@bronsonquick.com.au>
2019-01-05 13:47:30 +1100

    Add the down command

--- src/commands/down.js
+++ src/commands/down.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const execSync = require('child_process').execSync;
 const colors = require('colors');
+colors.setTheme( require( '../themes/logging.js' ) );
 
-// Run `docker-compose down`
+module.exports = function down() {
+    let cwd = process.cwd();
+    console.log( colors.warning( 'Stopping docker containers...' ) );
+    try {
+        execSync( `docker-compose down -d`, { stdio: 'inherit', cwd: cwd });
+    } catch (ex) {
+        console.log( colors.error( 'Failed to stop Docker containers.' ) );
+    }
+};
