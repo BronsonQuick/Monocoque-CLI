538bd424455ed99f49743048cb162fde39bf5d9d
Bronson Quick <bronson@bronsonquick.com.au>
2019-01-05 13:57:27 +1100

    Fix down command

--- src/commands/down.js
+++ src/commands/down.js
@@ -8,7 +8,7 @@ module.exports = function down() {
     let cwd = process.cwd();
     console.log( colors.warning( 'Stopping docker containers...' ) );
     try {
-        execSync( `docker-compose down -d`, { stdio: 'inherit', cwd: cwd });
+        execSync( `docker-compose down`, { stdio: 'inherit', cwd: cwd });
     } catch (ex) {
         console.log( colors.error( 'Failed to stop Docker containers.' ) );
     }
