3aa97d83406edd89174b2831fea385c6164f8edc
Bronson Quick <bronson@bronsonquick.com.au>
2019-05-30 15:15:09 +1000

    Add temporary command to supress a warning

--- src/commands/ssh.js
+++ src/commands/ssh.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const execSync = require('child_process').execSync;
 const colors = require('colors');
+colors.setTheme( require( '../themes/logging.js' ) );
 
-// Run `docker-compose exec ${container} bash`
+module.exports = function ssh() {
+    let cwd = process.cwd();
+    console.log( colors.warning( 'Logging into the container' ) );
+    try {
+        execSync( `docker-compose exec ${container} bash`, { stdio: 'inherit', cwd: cwd });
+    } catch (ex) {
+        console.log( colors.error( 'Failed to log in to the Docker container.' ) );
+    }
+};
